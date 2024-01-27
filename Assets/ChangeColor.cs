76f5e4d284fd589fae5278325cb77deda0dd67ad
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2024-01-27 15:04:58 -0800

    Add color selector

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    public DrawWithMouse drawWithMouse;
+
+    // Attach this function to the button's OnClick event in the Unity Editor
+    //the colors are red, blue, yellow, green, orange, purple, pink, black, white, gray, brown, cyan
+
+    public void ChangeColorToRed()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.red);
+        }
+    }
+    public void ChangeColorToBlue()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.blue);
+        }
+    }
+    public void ChangeColorToYellow()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.yellow);
+        }
+    }
+    public void ChangeColorToGreen()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.green);
+        }
+    }
+    public void ChangeColorToOrange()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(new Color(255f / 255f, 165f / 255f, 0f / 255f));
+        }
+    }
+    public void ChangeColorToPurple()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(new Color(128f / 255f, 0f, 128f / 255f));
+        }
+    }
+    public void ChangeColorToPink()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(new Color(255f / 255f, 192f / 255f, 203f / 255f));
+        }
+    }
+    public void ChangeColorToBlack()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.black);
+        }
+    }
+    public void ChangeColorToWhite()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.white);
+        }
+    }
+    public void ChangeColorToGray()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.gray);
+        }
+    }
+    public void ChangeColorToBrown()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(new Color(165f / 255f, 42f / 255f, 42f / 255f));
+        }
+    }
+    public void ChangeColorToCyan()
+    {
+        if (drawWithMouse != null)
+        {
+            drawWithMouse.ChangeColor(Color.cyan);
+        }
+    }
+    
+    
+}
--- Assets/DrawWithMouse.cs
+++ Assets/DrawWithMouse.cs
@@ -5,6 +5,7 @@ public class DrawWithMouse : MonoBehaviour
 {
     private List<LineRenderer> lines = new List<LineRenderer>();
     private List<Vector3> currentLinePositions = new List<Vector3>();
+    private Color currentColor = Color.black; // Default color is black
 
     [SerializeField]
     private float minDistance = 0.1f;
@@ -39,6 +40,7 @@ public class DrawWithMouse : MonoBehaviour
     {
         lines[lines.Count - 1].positionCount = currentLinePositions.Count;
         lines[lines.Count - 1].SetPositions(currentLinePositions.ToArray());
+        lines[lines.Count - 1].startColor = lines[lines.Count - 1].endColor = currentColor;
     }
 
     private void CreateNewLine()
@@ -46,8 +48,15 @@ public class DrawWithMouse : MonoBehaviour
         GameObject lineObject = new GameObject("Line");
         LineRenderer newLine = lineObject.AddComponent<LineRenderer>();
         newLine.startWidth = newLine.endWidth = width;
+        newLine.material = new Material(Shader.Find("Sprites/Default")); // Use a basic material for color support
 
         lines.Add(newLine);
         currentLinePositions.Clear();
+    }
+
+    // Example function for changing the color (you can call this function from UI buttons or other input)
+    public void ChangeColor(Color newColor)
+    {
+        currentColor = newColor;
     }
 }
