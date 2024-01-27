06ad4496d4bbd7daae8ffbde6dd54b11874db8d9
Nikman800 <76964182+Nikman800@users.noreply.github.com>
2024-01-27 12:40:20 -0800

    Added the ability to make multiple lines (no longer 1 continuous line)

--- Assets/DrawWithMouse.cs
+++ Assets/DrawWithMouse.cs
@@ -1,25 +1,19 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class DrawWithMouse : MonoBehaviour
 {
-    private LineRenderer line;
-    private Vector3 previousPosition;
+    private List<LineRenderer> lines = new List<LineRenderer>();
+    private List<Vector3> currentLinePositions = new List<Vector3>();
 
     [SerializeField]
     private float minDistance = 0.1f;
-    [SerializeField, Range(0.1f,2f)]
+    [SerializeField, Range(0.1f, 2f)]
     private float width = 0.1f;
 
     private void Start()
     {
-        line = GetComponent<LineRenderer>();
-        line.positionCount = 1;
-        previousPosition = transform.position;
-        line.startWidth = line.endWidth = width;
-
+        CreateNewLine();
     }
 
     private void Update()
@@ -29,23 +23,31 @@ public class DrawWithMouse : MonoBehaviour
             Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             currentPosition.z = 0f;
 
-            if (Vector3.Distance(currentPosition, previousPosition) > minDistance)
+            if (currentLinePositions.Count == 0 || Vector3.Distance(currentPosition, currentLinePositions[currentLinePositions.Count - 1]) > minDistance)
             {
-                if(previousPosition == transform.position)
-                {
-                    //it means that its the first point
-                    line.SetPosition(0, currentPosition);
-
-                }
-                else
-                {
-                    line.positionCount++;
-                    line.SetPosition(line.positionCount - 1, currentPosition);
-                }
-                
-                previousPosition = currentPosition;
+                currentLinePositions.Add(currentPosition);
+                UpdateLineRenderer();
             }
         }
-        
+        else if (Input.GetMouseButtonUp(0))
+        {
+            CreateNewLine();
+        }
+    }
+
+    private void UpdateLineRenderer()
+    {
+        lines[lines.Count - 1].positionCount = currentLinePositions.Count;
+        lines[lines.Count - 1].SetPositions(currentLinePositions.ToArray());
+    }
+
+    private void CreateNewLine()
+    {
+        GameObject lineObject = new GameObject("Line");
+        LineRenderer newLine = lineObject.AddComponent<LineRenderer>();
+        newLine.startWidth = newLine.endWidth = width;
+
+        lines.Add(newLine);
+        currentLinePositions.Clear();
     }
 }
